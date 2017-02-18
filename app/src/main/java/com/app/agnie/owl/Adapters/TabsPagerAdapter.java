39544dc1a8e0d207e61dd1b4c31ce054ba1ddbff
agnie <agnieszka.pula@gmail.com>
2017-02-18 19:03:50 +0100

    Added tabs to the dictionary activity

---
+++ app/src/main/java/com/app/agnie/owl/Adapters/TabsPagerAdapter.java
@@ -0,0 +1,41 @@
+package com.app.agnie.owl.Adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by agnie on 2/17/2017.
+ */
+
+public class TabsPagerAdapter extends FragmentPagerAdapter {
+    private final List<Fragment> fragmentList = new ArrayList<>();
+    private final List<String> fragmentTitleList = new ArrayList<>();
+
+    public TabsPagerAdapter(FragmentManager manager) {
+        super(manager);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        fragmentList.add(fragment);
+        fragmentTitleList.add(title);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentTitleList.get(position);
+    }
+}
--- app/src/main/java/com/app/agnie/owl/Dictionary.java
+++ app/src/main/java/com/app/agnie/owl/Dictionary.java
@@ -1,13 +1,52 @@
 package com.app.agnie.owl;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+import com.app.agnie.owl.Adapters.TabsPagerAdapter;
+import com.app.agnie.owl.Fragments.DictionaryPageOne;
+import com.app.agnie.owl.Fragments.DictionaryPageTwo;
+
 
 public class Dictionary extends AppCompatActivity {
+
+    PagerAdapter dictionaryPagerAdapter;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dictionary);
+        setupLayout();
     }
+
+    private void setupLayout(){
+        Toolbar toolbar = (Toolbar)findViewById(R.id.dictionary_toolbar);
+        setSupportActionBar(toolbar);
+        ViewPager viewPager = (ViewPager)findViewById(R.id.dictionary_viewpager);
+        setupTabPager(viewPager);
+        TabLayout tabs = (TabLayout) findViewById(R.id.dictionary_tabs);
+        tabs.setupWithViewPager(viewPager);
+    }
+
+    private void setupTabPager(ViewPager viewPager){
+        TabsPagerAdapter tabsPagerAdapter = new TabsPagerAdapter(getSupportFragmentManager());
+        tabsPagerAdapter.addFragment(new DictionaryPageOne(), "Daily Screech");
+        tabsPagerAdapter.addFragment(new DictionaryPageTwo(), "Dictionary List");
+        viewPager.setAdapter(tabsPagerAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_dictionary, menu);
+        return true;
+    }
+
 }
+
+
