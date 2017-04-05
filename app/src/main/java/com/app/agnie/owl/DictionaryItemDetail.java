3b8d80eb66d51151643865d492861fe08259168b
agnie <agnieszka.pula@gmail.com>
2017-04-05 19:06:47 +0200

    Implemented a basic drawer

--- app/src/main/java/com/app/agnie/owl/DictionaryItemDetail.java
+++ app/src/main/java/com/app/agnie/owl/DictionaryItemDetail.java
@@ -1,7 +1,12 @@
 package com.app.agnie.owl;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -16,6 +21,8 @@ public class DictionaryItemDetail extends AppCompatActivity {
     private DictionaryEntry selectedEntry;
     private FavouritePreference favouritePreference;
     private boolean favourite;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +36,7 @@ public class DictionaryItemDetail extends AppCompatActivity {
 
     private void setupLayout() {
         setupToolbar();
+        setupDrawer();
         DictionaryItemDetailFragment detailFragment = new DictionaryItemDetailFragment();
         Bundle arguments = new Bundle();
         arguments.putParcelable("selectedEntry", selectedEntry);
@@ -46,6 +54,38 @@ public class DictionaryItemDetail extends AppCompatActivity {
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
     }
+
+    private void setupDrawer(){
+        drawerLayout = (DrawerLayout)findViewById(R.id.item_detail_drawer_layout);
+        navigationView = (NavigationView)findViewById(R.id.nav_view);
+        navigationView.setCheckedItem(R.id.nav_dictionary);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                selectDrawerItem(item);
+                return true;
+            }
+        });
+    }
+
+    private void selectDrawerItem(MenuItem item){
+        Intent intent;
+        switch (item.getItemId()){
+            case R.id.nav_home:
+                intent = new Intent(this, OWLMain.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_dictionary:
+                intent = new Intent(this, Dictionary.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_favourites:
+            intent = new Intent(this, Favourites.class);
+            startActivity(intent);
+            break;
+        }
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.item_detail_toolbar, menu);
@@ -77,6 +117,9 @@ public class DictionaryItemDetail extends AppCompatActivity {
                     favouritePreference.addFavourite(getApplicationContext(), selectedEntry);
                     favourite = true;
                 }
+                return true;
+            case R.id.action_menu:
+                drawerLayout.openDrawer(GravityCompat.END);
                 return true;
         }
         return super.onOptionsItemSelected(item);
