bcb689695ce113cb961177c9f218e6f08698b8fe
agnie <agnieszka.pula@gmail.com>
2017-05-08 19:16:03 +0200

    Added more classes for tests

---
+++ app/src/main/java/com/app/agnie/owl/Fragments/TestsPageOne.java
@@ -0,0 +1,30 @@
+package com.app.agnie.owl.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.app.agnie.owl.R;
+
+public class TestsPageOne extends Fragment {
+
+    public TestsPageOne() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tests_page_one, container, false);
+    }
+
+
+}
--- app/src/main/java/com/app/agnie/owl/Tests.java
+++ app/src/main/java/com/app/agnie/owl/Tests.java
@@ -1,17 +1,21 @@
 package com.app.agnie.owl;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import junit.framework.Test;
+import com.app.agnie.owl.Adapters.TabsPagerAdapter;
+import com.app.agnie.owl.Fragments.FavouritesPageOne;
+import com.app.agnie.owl.Fragments.FavouritesPageTwo;
 
 public class Tests extends AppCompatActivity {
 
@@ -32,6 +36,17 @@ public class Tests extends AppCompatActivity {
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
         setupDrawer();
+        ViewPager viewPager = (ViewPager) findViewById(R.id.tests_viewpager);
+        setupTabPager(viewPager);
+        TabLayout tabs = (TabLayout) findViewById(R.id.tests_tabs);
+        tabs.setupWithViewPager(viewPager);
+    }
+
+    private void setupTabPager(ViewPager viewPager) {
+        TabsPagerAdapter tabsPagerAdapter = new TabsPagerAdapter(getSupportFragmentManager());
+        tabsPagerAdapter.addFragment(new FavouritesPageOne(), "Words");
+        tabsPagerAdapter.addFragment(new FavouritesPageTwo(), "Lessons");
+        viewPager.setAdapter(tabsPagerAdapter);
     }
 
     @Override
