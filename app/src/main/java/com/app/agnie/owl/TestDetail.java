f5ddbffbeec37393067183a2585605e4f8e7754b
agnie <agnieszka.pula@gmail.com>
2017-05-11 16:59:35 +0200

    Added a class for quiz question

--- app/src/main/java/com/app/agnie/owl/TestDetail.java
+++ app/src/main/java/com/app/agnie/owl/TestDetail.java
@@ -1,13 +1,107 @@
 package com.app.agnie.owl;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.app.agnie.owl.Fragments.LessonDetailFragment;
 
 public class TestDetail extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_detail);
+        setupLayout();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_default, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            case R.id.action_menu:
+                drawerLayout.openDrawer(GravityCompat.END);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupLayout() {
+        setupToolbar();
+        setupDrawer();
+//        LessonDetailFragment detailFragment = new LessonDetailFragment();
+//        Bundle arguments = new Bundle();
+//        arguments.putParcelable("selectedTest", selectedTest);
+//        detailFragment.setArguments(arguments);
+//        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+//        fragmentTransaction.replace(R.id.lesson_frame_placeholder, detailFragment);
+//        fragmentTransaction.commit();
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.test_detail_toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+    }
+
+    private void setupDrawer() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.test_detail_drawer_layout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setCheckedItem(R.id.nav_tests);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                selectDrawerItem(item);
+                return true;
+            }
+        });
+    }
+
+    private void selectDrawerItem(MenuItem item) {
+        Intent intent;
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                intent = new Intent(this, OWLMain.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_dictionary:
+                intent = new Intent(this, Dictionary.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_favourites:
+                intent = new Intent(this, Favourites.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_lessons:
+                intent = new Intent(this, Lessons.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_tests:
+                intent = new Intent(this, Tests.class);
+                startActivity(intent);
+                break;
+        }
+    }
+
 }
