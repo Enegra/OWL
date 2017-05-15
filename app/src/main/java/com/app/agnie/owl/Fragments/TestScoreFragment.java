bb9dfed82603c7a5efb187efda2f5c109e6309d5
agnie <agnieszka.pula@gmail.com>
2017-05-16 00:43:28 +0200

    Added score fragment

---
+++ app/src/main/java/com/app/agnie/owl/Fragments/TestScoreFragment.java
@@ -0,0 +1,28 @@
+package com.app.agnie.owl.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.app.agnie.owl.R;
+
+
+public class TestScoreFragment extends Fragment {
+
+
+    public TestScoreFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_test_score, container, false);
+    }
+
+}
