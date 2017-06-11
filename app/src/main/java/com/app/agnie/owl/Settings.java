5cb2f49d1b9d516bda8a2d9ddbe729f459b4a8b3
agnie <agnieszka.pula@gmail.com>
2017-06-11 16:15:13 +0200

    Fixed a crash

--- app/src/main/java/com/app/agnie/owl/Settings.java
+++ app/src/main/java/com/app/agnie/owl/Settings.java
@@ -179,17 +179,6 @@ public class Settings extends AppCompatActivity{
         protected void onPostExecute(Void result) {
             super.onPostExecute(result);
             progressDialog.dismiss();
-            dataSource.open();
-            if(updatesAvailable[0] || updatesAvailable[1] || updatesAvailable[2]){
-                dataSource.insertDictionaryValues(Settings.this, updatedDatabase.get(0));
-            }
-            if(updatesAvailable[3]){
-                dataSource.insertLessonValues(Settings.this, updatedDatabase.get(1).get(0));
-            }
-            if(updatesAvailable[4] || updatesAvailable[5] ||updatesAvailable[6]){
-                dataSource.insertTestValues(Settings.this, updatedDatabase.get(2));
-            }
-            dataSource.close();
         }
 
         @Override
@@ -245,6 +234,17 @@ public class Settings extends AppCompatActivity{
             }
             editor.apply();
             updatedDatabase = getUpdatedDatabase(preferencesArray, updatesAvailable);
+            dataSource.open();
+            if(updatesAvailable[0] || updatesAvailable[1] || updatesAvailable[2]){
+                dataSource.insertDictionaryValues(Settings.this, updatedDatabase.get(0));
+            }
+            if(updatesAvailable[3]){
+                dataSource.insertLessonValues(Settings.this, updatedDatabase.get(1).get(0));
+            }
+            if(updatesAvailable[4] || updatesAvailable[5] ||updatesAvailable[6]){
+                dataSource.insertTestValues(Settings.this, updatedDatabase.get(2));
+            }
+            dataSource.close();
             return null;
         }
     }
