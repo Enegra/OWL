d5b035bed96653ae79521297bb09880d91d06c06
agnie <agnieszka.pula@gmail.com>
2017-06-15 12:10:23 +0200

    Added some test classes

--- app/src/androidTest/java/com/app/agnie/owl/SettingsTest.java
+++ app/src/androidTest/java/com/app/agnie/owl/SettingsTest.java
@@ -2,9 +2,6 @@ package com.app.agnie.owl;
 
 import org.junit.Test;
 
-/**
- * Created by agnie on 6/15/2017.
- */
 public class SettingsTest {
     @Test
     public void onUpdateButtonClick() throws Exception {
